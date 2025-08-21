927a547f439a76c3f34e0ea8ff806227a0621ae6
Kuncoro Aji Nugroho <41549851+kuncoroajinugroho@users.noreply.github.com>
2025-08-21 08:19:08 +0700

    Update fct_user_transaction_behavior.sql

--- fct_user_transaction_behavior.sql
+++ fct_user_transaction_behavior.sql
@@ -1,52 +1,53 @@
-
--- Create FCT Table: fct_user_transaction_behavior
-
-WITH transaction_per_user AS (
-  SELECT
-    client_id AS user_id,
-    COUNT(*) AS transaction_per_user
-  FROM `project.dataset.transaction_data`
-  GROUP BY client_id
-)
-
+CREATE OR REPLACE TABLE `data-analyst-mandiri-sekuritas.Technical_Test_1.fct_user_transaction_behavior` AS
 SELECT
-  -- User data
+  -- From users_data
   u.id AS user_id,
   u.gender,
   u.current_age,
+  CASE
+    WHEN u.current_age BETWEEN 18 AND 26 THEN 'Gen Z (18-26)'
+    WHEN u.current_age BETWEEN 27 AND 43 THEN 'Millennials (27-43)'
+    WHEN u.current_age BETWEEN 44 AND 59 THEN 'Gen X (44-59)'
+    WHEN u.current_age >= 60 THEN 'Boomers (60+)'
+    ELSE 'Unknown'
+  END age_segment,
+  u.birth_year,
+  u.birth_month,
   u.per_capita_income,
   u.yearly_income,
   u.total_debt,
   u.credit_score,
   u.num_credit_cards,
-
-  -- Card data
+  u.latitude,
+  u.longitude,
+  
+  -- From cards_data
   c.id AS card_id,
   c.card_type,
   c.card_brand,
+  c.num_cards_issued,
   c.credit_limit,
   c.has_chip,
   c.card_on_dark_web,
-
-  -- Transaction data
+  c.acct_open_date,
+  
+  -- From transaction_data
   t.id AS transaction_id,
-  FORMAT_TIMESTAMP('%Y-%m-%d %H:%M:%S', t.date) AS transaction_date,
+  FORMAT_TIMESTAMP('%Y-%m-%d %H:%M:%S', date) AS transaction_date,
   t.amount AS transaction_amount,
   t.use_chip,
   t.merchant_id,
+  t.merchant_city,
+  t.merchant_state,
+  t.zip,
   t.mcc,
-  t.errors,
+  t.errors
+  
+FROM `data-analyst-mandiri-sekuritas.Technical_Test_1.transaction_data` t
+LEFT JOIN `data-analyst-mandiri-sekuritas.Technical_Test_1.cards_data` c
+  ON t.card_id = c.id
+LEFT JOIN `data-analyst-mandiri-sekuritas.Technical_Test_1.users_data` u
+  ON t.client_id = u.id;
 
-  -- Behavior segmentation
-  tp.transaction_per_user,
-  CASE
-    WHEN tp.transaction_per_user = 1 THEN 'One-Time Buyer'
-    WHEN tp.transaction_per_user BETWEEN 2 AND 10 THEN 'Occasional Buyer'
-    WHEN tp.transaction_per_user > 10 THEN 'Frequent Buyer'
-    ELSE 'Unknown'
-  END AS repeat_user_segment
 
-FROM `project.dataset.transaction_data` t
-LEFT JOIN `project.dataset.cards_data` c ON t.card_id = c.id
-LEFT JOIN `project.dataset.users_data` u ON t.client_id = u.id
-LEFT JOIN transaction_per_user tp ON t.client_id = tp.user_id;
+
